96c05da903c0d8ad689ceb5fad150cbf3d12d565
taiatulislam <taiatulislamapon@gmail.com>
2023-11-29 02:07:02 +0600

    contest delete by admin

--- index.js
+++ index.js
@@ -130,6 +130,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/allContest/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await contests.deleteOne(query);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
